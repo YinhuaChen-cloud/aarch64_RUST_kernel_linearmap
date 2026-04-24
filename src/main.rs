50b1bd13d16a985a16d1686862ce76b10bfbc899
YinhuaChen-cloud <229676342@qq.com>
2026-04-24 12:02:18 +0800

    split uart.rs

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,14 @@
 use core::panic::PanicInfo;
 
 mod start;
-
-const PL011_BASE: usize = 0x0900_0000;
-const UART_DR: *mut u32 = PL011_BASE as *mut u32;
-const UART_FR: *const u32 = (PL011_BASE + 0x18) as *const u32;
-const UART_FR_TXFF: u32 = 1 << 5;
+mod uart;
 
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
-    for &byte in b"hello world\r\n" {
-        uart_write(byte);
-    }
+    uart::puts(b"hello world\r\n");
 
     loop {
         core::hint::spin_loop();
-    }
-}
-
-fn uart_write(byte: u8) {
-    unsafe {
-        while core::ptr::read_volatile(UART_FR) & UART_FR_TXFF != 0 {}
-        core::ptr::write_volatile(UART_DR, byte as u32);
     }
 }
 
---
+++ src/uart.rs
@@ -0,0 +1,17 @@
+const PL011_BASE: usize = 0x0900_0000;
+const UART_DR: *mut u32 = PL011_BASE as *mut u32;
+const UART_FR: *const u32 = (PL011_BASE + 0x18) as *const u32;
+const UART_FR_TXFF: u32 = 1 << 5;
+
+pub fn puts(s: &[u8]) {
+    for &byte in s {
+        write_byte(byte);
+    }
+}
+
+pub fn write_byte(byte: u8) {
+    unsafe {
+        while core::ptr::read_volatile(UART_FR) & UART_FR_TXFF != 0 {}
+        core::ptr::write_volatile(UART_DR, byte as u32);
+    }
+}
